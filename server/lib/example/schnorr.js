1afa7d22eed3f53e1e4f07c68a1a6e30c59fa295
Nayotama Pradipta <n4y0t4m4@gmail.com>
2024-05-22 20:13:45 +0700

    feat: verify digital signature (schnorr)

--- server/lib/example/schnorr.js
+++ server/lib/example/schnorr.js
@@ -1,4 +1,4 @@
-const { generatePrivateKey, generatePublicKey, hash, generate_DS } = require('../schnorr')
+const { generatePrivateKey, generatePublicKey, generate_DS, verify_DS } = require('../schnorr')
 
 
 const priv_key = generatePrivateKey();
@@ -6,7 +6,8 @@ const pub_key = generatePublicKey(priv_key);
 
 const msg = "Tugas-5-Kriptografi";
 const signature = generate_DS(msg, priv_key);
-
+const verify = verify_DS(msg, signature, pub_key)
 console.log("Private Key: ", priv_key);
 console.log("Public Key: ", pub_key);
 console.log("Signature: ", signature);
+console.log("Verify Status: ", verify)
--- server/lib/schnorr.js
+++ server/lib/schnorr.js
@@ -19,28 +19,53 @@ if (modPow(alpha, q, p) !== 1n) {
 }
 
 function generatePrivateKey(){
-    return BigInt('0x' + crypto.randomBytes(32).toString('hex')) % q;
+    let priv_key; 
+    do {
+        priv_key = BigInt('0x' + crypto.randomBytes(32).toString('hex')) % q;
+    } while (priv_key === 0n);
+    return priv_key;
 }
 
 function generatePublicKey(s){
-    return alpha ** s % p;
+    let key; 
+    do {
+        key = modPow(alpha, -s, p);
+    } while (key === 0n);
+    return key;
 }
 
-function hash(data) {
+function hash(message, x) {
+    const messageBuffer = Buffer.from(message, 'utf8');
+    const xBuffer = Buffer.from(x.toString(), 'utf8');
+    const data = Buffer.concat([messageBuffer, xBuffer]);
     return BigInt('0x' + crypto.createHash('sha256').update(data).digest('hex'));
 }
 
 function generate_DS(message, s){
     const r = generatePrivateKey();
-    const x = alpha ** r % p;
-    const e = hash(message + x.toString())
+    const x = modPow(alpha, r, p);
+    const e = hash(message, x)
     const y = (r + s*e) % q;
-    return {e:e, y:y};
+    return { e, y };
 }
 
+function verify_DS(message, signature, public_key){
+    const {e, y} = signature;
+    const term1 = modPow(alpha, y, p);
+    const term2 = modPow(public_key, e, p);
+    const x_aksen = (term1 * term2) % p; 
+    const e_aksen = hash(message, x_aksen)
+    return e === e_aksen
+}
+
+
 module.exports = {
+    get_P,
+    get_Q,
+    get_alpha,
     generatePrivateKey,
     generatePublicKey,
     hash,
-    generate_DS
+    generate_DS,
+    verify_DS
 }
